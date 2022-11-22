e31e55d25c2097792775aa3b3a3ee80d1b4f76ac
Nikita Sapogov <sapogov.n@citilink.ru>
2022-11-22 22:40:00 +0300

    initialized repository

---
+++ plugins/wotlk/combat_rotation/deathknight/unholy/v1/01_amstaffix_deathknight_unholy_loader.lua
@@ -0,0 +1,96 @@
+local ID = "CR>DK/U"
+local LIB_LINK = "https://raw.githubusercontent.com/AmsTaFFix/gmr-stuff/main/backside/plugins/custom/00_amstlib.lua"
+local ROTATION_LINK = "{LINK TO ENCRYPTED VERSION}"
+---@type DeathKnightUnholyV1Config
+local Config = {
+    ---Toggle debug mode. Turn on, if you encounter some issues and want to deal with it, or record a video and send
+    ---to author.
+    debug = true,
+    ---Use standard CombatRotation pluggable function. Change only if you know what you are doing.
+    useCombatRotationLauncher = true,
+    ---Use online loading feature to get last updates
+    onlineLoad = false,
+    ---Character names to force load that rotation
+    forceLoadForCharacters = {},
+
+    ---Min HP to cast Icebound Fortitude
+    iceboundFortitudeHpUse = 50,
+    bloodBoilEnabled = true,
+    ---Should bot use only blood runes for blood-runes offensive spells like Blood Boil, Heart/Blood Strike
+    useBloodFillersWithBloodRunesOnly = false,
+    ---Min enemies to start using Blood Boil instead off Blood/Hearth Strike
+    bloodBoilMinEnemies = 3,
+    ---Default presence
+    --- - 1:blood;
+    --- - 2:frost;
+    --- - 3:unholy;
+    defaultPresence = 1,
+    ---Change presence on frost, if HP < X. Change it to 0 to turn off
+    minHpToChangeToFrostPresence = 60,
+
+    useStrangulateToInterruptCasts = true,
+    useDeathGripToInterruptCasts = true,
+
+    useDeathAndDecay = false,
+    useDeathAndDecayMinEnemies = 3,
+
+    useDeathCoilOnEnemy = true,
+    useDeathCoilOnEnemyMaxEnemies = 100,
+
+    useCorpseExplosion = false,
+    useCorpseExplosionMinEnemies = 3,
+
+    useGhoulFrenzy = true,
+
+    --- should use death strike in some circumstances
+    useDeathStrike = false,
+    --- Using Death strike when HP < N
+    useDeathStrikeMaxHp = 60,
+
+    useTrinket1 = false,
+    useTrinket1Type = 1, -- 1:self-buff, 2:target-harmful, 3:aoe-harmful
+
+    useTrinket2 = false,
+    useTrinket2Type = 1, -- 1:self-buff, 2:target-harmful, 3:aoe-harmful
+}
+
+local function printDbg(msg)
+    if Config.debug then
+        GMR.Print("[" .. ID .. "][DEBUG] " .. msg)
+    end
+end
+
+do
+    local msgPrefix = "[" .. ID .. "] "
+    if Config.onlineLoad then
+        GMR.SendHttpRequest({
+            Url = LIB_LINK,
+            Method = "Get",
+            Callback = function(content)
+                printDbg("library has been downloaded, now we'll try to execute it")
+                RunScript(content)
+                if not amstlib then
+                    GMR.Print(msgPrefix .. "AmsTaFFix' Lib do not initialized properly")
+                    return
+                end
+
+                printDbg("library successfully initialized, start work with rotation")
+                local cr = amstlib:getCombatRotation(ID)
+                cr:prepare(Config)
+                printDbg("rotation has been prepared, start loading the file with rotation")
+                cr:load(ROTATION_LINK)
+            end
+        })
+    else
+        GMR.Print(msgPrefix .. "Offline loading of rotation initiated")
+        if amstlib then
+            local cr = amstlib:getCombatRotation(ID)
+            cr:prepare(Config)
+            if Config.onlineLoad then
+                cr:load(ROTATION_LINK)
+            end
+        else
+            GMR.Print(msgPrefix .. "AmsTaFFix' Lib do not initialized properly")
+        end
+    end
+end
