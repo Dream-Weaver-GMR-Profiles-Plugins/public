f6d4c98f9178c64f9aaf8c3ba7b4c5379fd52f20
Nikita <amstaffix@gmail.com>
2023-01-09 23:20:59 +0700

    prepared beastmastery rotation

--- plugins/retail/combat_rotation/hunter/beastmastery/v1/01_amstaffix_hunter_beastmastery_loader.lua
+++ plugins/retail/combat_rotation/hunter/beastmastery/v1/01_amstaffix_hunter_beastmastery_loader.lua
@@ -5,16 +5,19 @@ local ROTATION_LINK = "https://raw.githubusercontent.com/Dream-Weaver-GMR-Profil
 local Config = {
     ---Toggle debug mode. Turn on, if you encounter some issues and want to deal with it, or record a video and send
     ---to author.
-    debug = true,
+    debug = false,
     collectMetrics = false,
     ---Use standard CombatRotation pluggable function. Change only if you know what you are doing.
     useCombatRotationLauncher = true,
     ---Use online loading feature to get last updates
-    onlineLoad = false,
+    onlineLoad = true,
     ---Character names to force load that rotation
     forceLoadForCharacters = {},
-    
+
     minEnemiesToAoe = 3,
+
+    useCobraShotAt = 70,
+    useSteelTrap = true,
 
     useTrinket1 = false,
     useTrinket1Type = 1, -- 1:self-buff, 2:target-harmful, 3:aoe-harmful
