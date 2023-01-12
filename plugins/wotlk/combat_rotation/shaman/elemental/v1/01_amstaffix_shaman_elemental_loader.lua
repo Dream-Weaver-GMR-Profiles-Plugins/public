36b12f5582c7802e7ec42f983f03732e7c43c999
Nikita <amstaffix@gmail.com>
2023-01-12 17:34:06 +0700

    changed id of ele shaman cr

--- plugins/wotlk/combat_rotation/shaman/elemental/v1/01_amstaffix_shaman_elemental_loader.lua
+++ plugins/wotlk/combat_rotation/shaman/elemental/v1/01_amstaffix_shaman_elemental_loader.lua
@@ -1,4 +1,4 @@
-local ID = "CR/W>S/E"
+local ID = "CR/W>S/El"
 local LIB_LINK = "https://raw.githubusercontent.com/Dream-Weaver-GMR-Profiles-Plugins/public/master/plugins/wotlk/custom/00_amstlib.gmr"
 local ROTATION_LINK = "https://raw.githubusercontent.com/Dream-Weaver-GMR-Profiles-Plugins/public/master/plugins/wotlk/combat_rotation/shaman/elemental/v1/90_amstaffix_shaman_elemental_rotation.gmr"
 ---@type ShamanElementalV1Config
