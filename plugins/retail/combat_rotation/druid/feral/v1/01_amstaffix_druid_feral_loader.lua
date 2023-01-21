f7a9f927f6fbf7291ca47d52b3d59c40f5909860
Nikita <amstaffix@gmail.com>
2023-01-21 20:43:43 +0700

    finished feral rotation

--- plugins/retail/combat_rotation/druid/feral/v1/01_amstaffix_druid_feral_loader.lua
+++ plugins/retail/combat_rotation/druid/feral/v1/01_amstaffix_druid_feral_loader.lua
@@ -15,6 +15,24 @@ local Config = {
     ---Use all bursts when they off cooldown
     useBurstAlways = true,
 
+    useRegrowthWithSwiftnessMinHP = 90,
+    useThrashMinEnemies = 3,
+    usePrimalWrathMinEnemies = 3,
+    useSwipeMinEnemies = 3,
+    useRakeToSpreadMinEnemies = 2, -- work always with bloodtalons
+    useRakeToSpreadMinHP = 70,
+    useMoonfireToSpreadMinEnemies = 2, -- work always with bloodtalons
+    useMoonfireToSpreadMinHP = 70,
+    useSurvivalInstinctsMinHP = 30,
+    useBarskinMinHP = 70,
+    useFrenziedRegenerationMinHP = 50,
+    useNaturesVigil = false,
+    useRenewalMinHP = 70,
+    useSkullBash = true,
+    useWildCharge = true,
+    useProwl = true,
+    useSwiftmendMinHP = 80,
+
     useTrinket1 = false,
     useTrinket1Type = 1, -- 1:self-buff, 2:target-harmful, 3:aoe-harmful
 
