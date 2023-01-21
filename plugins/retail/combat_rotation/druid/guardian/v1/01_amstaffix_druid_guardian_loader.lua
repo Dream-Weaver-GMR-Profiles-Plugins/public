2a7b3ab059e6d78944651e3de6a2d363158927af
Nikita <amstaffix@gmail.com>
2023-01-22 00:15:12 +0700

    added guardian rotation

---
+++ plugins/retail/combat_rotation/druid/guardian/v1/01_amstaffix_druid_guardian_loader.lua
@@ -0,0 +1,77 @@
+local ID = "CR/DF>D/G"
+local LIB_LINK = "https://raw.githubusercontent.com/Dream-Weaver-GMR-Profiles-Plugins/public/master/plugins/wotlk/custom/00_amstlib.gmr"
+local ROTATION_LINK = "https://raw.githubusercontent.com/Dream-Weaver-GMR-Profiles-Plugins/public/master/plugins/retail/combat_rotation/druid/guardian/v1/90_amstaffix_druid_guardian_rotation.gmr"
+---@type DFDruidGuardianV1Config
+local Config = {
+    ---Toggle debug mode. Turn on, if you encounter some issues and want to deal with it, or record a video and send
+    ---to author.
+    debug = true,
+    ---Use standard CombatRotation pluggable function. Change only if you know what you are doing.
+    useCombatRotationLauncher = true,
+    ---Use online loading feature to get last updates
+    onlineLoad = false,
+    ---Character names to force load that rotation
+    forceLoadForCharacters = {},
+    ---Use all bursts when they off cooldown
+    useBurstAlways = true,
+
+    useMaulWithViciousCycleMinHP = 70,
+    useBristlingFurMinHP = 60,
+    usePulverizeMinHP = 55,
+    usePulverizeAsPartOfBurst = false,
+    useSurvivalInstinctsMinHP = 50,
+    useFrenziedRegenerationMinHP = 65,
+    useRegrowthUnderDreamOfCenariusMinHP = 80,
+    useRageOfTheSleeperMinHP = 85,
+    useBarskinMinHP = 80,
+    useRenewalMinHP = 70,
+    useNaturesVigil = false,
+    useSkullBash = true,
+
+    useTrinket1 = false,
+    useTrinket1Type = 1, -- 1:self-buff, 2:target-harmful, 3:aoe-harmful
+
+    useTrinket2 = false,
+    useTrinket2Type = 1, -- 1:self-buff, 2:target-harmful, 3:aoe-harmful
+}
+
+local function printDbg(msg)
+    if Config.debug then
+        GMR.Print("[" .. ID .. "][DEBUG] " .. msg)
+    end
+end
+
+do
+    local msgPrefix = "[" .. ID .. "] "
+    if Config.onlineLoad then
+        GMR.SendHttpRequest({
+            Url = LIB_LINK,
+            Method = "Get",
+            Callback = function(content)
+                printDbg("library has been downloaded, now we'll try to execute it")
+                GMR.RunEncryptedScript(content)
+                if not amstlib then
+                    GMR.Print(msgPrefix .. "AmsTaFFix' Lib do not initialized properly")
+                    return
+                end
+
+                printDbg("library successfully initialized, start work with rotation")
+                local cr = amstlib:getCombatRotation(ID)
+                cr:prepare(Config)
+                printDbg("rotation has been prepared, start loading the file with rotation")
+                cr:loadEncrypted(ROTATION_LINK)
+            end
+        })
+    else
+        GMR.Print(msgPrefix .. "Offline loading of rotation initiated")
+        if amstlib then
+            local cr = amstlib:getCombatRotation(ID)
+            cr:prepare(Config)
+            if Config.onlineLoad then
+                cr:loadEncrypted(ROTATION_LINK)
+            end
+        else
+            GMR.Print(msgPrefix .. "AmsTaFFix' Lib do not initialized properly")
+        end
+    end
+end
