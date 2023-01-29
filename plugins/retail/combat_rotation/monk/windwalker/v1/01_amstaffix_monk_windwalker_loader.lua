6d5f800b9559ce19864a035574b41a2a24be2bc2
Nikita <amstaffix@gmail.com>
2023-01-29 12:07:13 +0700

    completed single, cleave and aoe rotaions for windwalker

---
+++ plugins/retail/combat_rotation/monk/windwalker/v1/01_amstaffix_monk_windwalker_loader.lua
@@ -0,0 +1,64 @@
+local ID = "CR/DF>M/W"
+local LIB_LINK = "https://raw.githubusercontent.com/Dream-Weaver-GMR-Profiles-Plugins/public/master/plugins/wotlk/custom/00_amstlib.gmr"
+local ROTATION_LINK = "https://raw.githubusercontent.com/Dream-Weaver-GMR-Profiles-Plugins/public/master/plugins/retail/combat_rotation/monk/windwalker/v1/90_amstaffix_monk_windwalker_rotation.gmr"
+---@type DFMonkWindwalkerV1Config
+local Config = {
+    ---Toggle debug mode. Turn on, if you encounter some issues and want to deal with it, or record a video and send
+    ---to author.
+    debug = true,
+    ---Use standard CombatRotation pluggable function. Change only if you know what you are doing.
+    useCombatRotationLauncher = true,
+    ---Use online loading feature to get last updates
+    onlineLoad = false,
+    ---Character names to force load that rotation
+    forceLoadForCharacters = {},
+    ---Use all bursts when they off cooldown
+    useBurstAlways = true,
+
+    useTrinket1 = false,
+    useTrinket1Type = 1, -- 1:self-buff, 2:target-harmful, 3:aoe-harmful
+
+    useTrinket2 = false,
+    useTrinket2Type = 1, -- 1:self-buff, 2:target-harmful, 3:aoe-harmful
+}
+
+local function printDbg(msg)
+    if Config.debug then
+        GMR.Print("[" .. ID .. "][DEBUG] " .. msg)
+    end
+end
+
+if UnitClassBase("player") == "MONK" and (GetSpecialization() == 5 or GetSpecialization() == 3) then
+    local msgPrefix = "[" .. ID .. "] "
+    if Config.onlineLoad then
+        GMR.SendHttpRequest({
+            Url = LIB_LINK,
+            Method = "Get",
+            Callback = function(content)
+                printDbg("library has been downloaded, now we'll try to execute it")
+                GMR.RunEncryptedScript(content)
+                if not amstlib then
+                    GMR.Print(msgPrefix .. "AmsTaFFix' Lib do not initialized properly")
+                    return
+                end
+
+                printDbg("library successfully initialized, start work with rotation")
+                local cr = amstlib:getCombatRotation(ID)
+                cr:prepare(Config)
+                printDbg("rotation has been prepared, start loading the file with rotation")
+                cr:loadEncrypted(ROTATION_LINK)
+            end
+        })
+    else
+        GMR.Print(msgPrefix .. "Offline loading of rotation initiated")
+        if amstlib then
+            local cr = amstlib:getCombatRotation(ID)
+            cr:prepare(Config)
+            if Config.onlineLoad then
+                cr:loadEncrypted(ROTATION_LINK)
+            end
+        else
+            GMR.Print(msgPrefix .. "AmsTaFFix' Lib do not initialized properly")
+        end
+    end
+end
