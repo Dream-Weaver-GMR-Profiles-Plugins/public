d4f7f1495352e2acde90a4e25dcacad05c02ea80
evagorasGMR <evagoras@levent.is>
2023-02-21 18:49:17 +0000

    updated

--- plugins/retail/Questing/EvagorasTheWakingShores_autoloader.lua
+++
@@ -1,5 +0,0 @@
-GMR.SendRequestAndThen("raw.githubusercontent.com/Dream-Weaver-GMR-Profiles-Plugins/public/master/plugins/retail/Questing/EvagorasTheWakingShores.gmr", true,
-    function(cipher)
-        GMR.RunEncryptedScript(cipher)
-    end
-)
