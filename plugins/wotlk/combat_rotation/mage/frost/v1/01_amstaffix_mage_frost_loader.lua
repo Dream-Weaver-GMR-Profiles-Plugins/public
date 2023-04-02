1e34e8fca6688bd36a66298d3827eb55111f3761
Nikita <amstaffix@gmail.com>
2023-04-02 14:25:12 +0700

    added usage of ice lance spell on frozen targets

--- plugins/wotlk/combat_rotation/mage/frost/v1/01_amstaffix_mage_frost_loader.lua
+++ plugins/wotlk/combat_rotation/mage/frost/v1/01_amstaffix_mage_frost_loader.lua
@@ -27,6 +27,7 @@ local Config = {
     useConeOfColdOnlyWhileMoving = true,
     useFrostNovaDistance = 10,
     useArcaneIntellect = true,
+    useIceLance = true,
 
     useTrinket1 = false,
     useTrinket1Type = 1, -- 1:self-buff, 2:target-harmful, 3:aoe-harmful
